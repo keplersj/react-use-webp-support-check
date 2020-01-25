69abe43ed01ae1caff4d333c32bd02101b39e419
Kepler Sticka-Jones <kepler+git@stickajones.org>
2020-01-25 15:17:17 -0700

    test(eslint): use new auto prefix

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ export function useWebPSupportCheck(): boolean {
   const [{ webP: supportsWebP }, setWebPSupport] = useState({ webP: false });
 
   useEffect(() => {
+    // eslint-disable-next-line unicorn/consistent-function-scoping
     const checkForSupport = async (): Promise<void> => {
       const browserSupportsWebP = await webPCheck;
 
