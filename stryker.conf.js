f5613737ee5baf3ba6d537075dff8fd010353117
Kepler Sticka-Jones <kepler+git@stickajones.org>
2021-01-04 22:29:13 -0700

    chore(devDeps): update

--- stryker.conf.js
+++ stryker.conf.js
@@ -2,18 +2,16 @@
 const path = require("path");
 
 // Please see the handbook for more information: https://github.com/stryker-mutator/stryker-handbook/blob/master/stryker/guides/react.md#react
-module.exports = function (config) {
-  config.set({
-    mutate: ["src/**/*.ts", "!src/__tests__/**/*.ts"],
-    mutator: "typescript",
-    testRunner: "jest",
-    reporters: ["progress", "clear-text", "html"],
-    coverageAnalysis: "off",
-    jest: {
-      projectType: "custom",
-      // Only use the unit test project
-      config: require(path.resolve(__dirname, "./jest.config.js")).projects[0],
-      enableFindRelatedTests: true,
-    },
-  });
+module.exports = {
+  mutate: ["src/**/*.ts", "!src/__tests__/**/*.ts"],
+  testRunner: "jest",
+  reporters: ["progress", "clear-text", "html"],
+  coverageAnalysis: "off",
+  jest: {
+    projectType: "custom",
+    // Only use the unit test project
+    config: require(path.resolve(__dirname, "./jest.config.js")).projects[0],
+    configFile: path.resolve(__dirname, "./jest.config.js"),
+    enableFindRelatedTests: true,
+  },
 };
